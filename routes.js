9e0a766a8b4efe8b403e412386baceb220637228
Daniel Moser <danielmoser49@gmail.com>
2016-03-02 17:59:13 -0600

    Add lots of css

--- routes.js
+++ routes.js
@@ -6,15 +6,30 @@ Router.configure({
 });
 
 Router.route('/', function() {
-    this.render('welcome', {
+    this.render('login', {
         to: "main"
-    });
+    })
+    // this.render('navigation', {
+    //     to: "navbar"
+    // });
 });
 
-Router.route('/login', function() {
-    this.render('login');
-    this.render('navigation', {
-        to: "navbar"
-    });
+// Router.route('/login', function() {
+//     this.render('login');
+//     this.render('navigation', {
+//         to: "navbar"
+//     });
+// });
+
+Router.route('/budget', function() {
+    this.render('budget', {
+        to: "main"
+    })
+});
+
+Router.route('/accounts', function() {
+    this.render('accounts', {
+        to: "main"
+    })
 });
 
